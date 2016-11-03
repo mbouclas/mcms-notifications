90fd64aac889f6c0a710b3f47c7b1a05c7366b3f
Michael Bouclas <mbouclas@gmail.com>
2016-11-03 23:12:38 +0200

    Initial commit

--- src/Installer/AfterUpdate/CreateMissingTable.php
+++ src/Installer/AfterUpdate/CreateMissingTable.php
@@ -4,12 +4,21 @@ namespace Mcms\Notifications\Installer\AfterUpdate;
 
 
 use IdeaSeven\Core\Models\UpdatesLog;
+use IdeaSeven\FrontEnd\Installer\AfterUpdate\CreateMissingTable\CreateNotificationsTables;
 use Illuminate\Console\Command;
 
 class CreateMissingTable
 {
     public function handle(Command $command, UpdatesLog $item)
     {
+        $classes = [
+            CreateNotificationsTables::class
+        ];
+
+        foreach ($classes as $class) {
+            (new $class())->handle($command);
+        }
+
         $item->result = true;
         $item->save();
         $command->comment('All done in CreateMissingTable');
---
+++ src/Installer/AfterUpdate/CreateMissingTable/CreateNotificationsTables.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace IdeaSeven\FrontEnd\Installer\AfterUpdate\CreateMissingTable;
+
+use Illuminate\Console\Command;
+use Schema;
+
+class CreateNotificationsTables
+{
+    public function handle(Command $command)
+    {
+
+        if ( ! Schema::hasTable('notifications')){
+            $file = '2016_09_28_090035_create_notifications_table.php';
+            $targetFile = database_path("migrations/{$file}");
+            if ( ! \File::exists($targetFile)){
+                \File::copy(__DIR__ . "/../../../../database/migrations/{$file}", $targetFile);
+            }
+            $command->call('migrate');
+        }
+
+        if ( ! Schema::hasTable('user_notifications')){
+            $file = '2016_09_28_090331_user_notification_table.php';
+            $targetFile = database_path("migrations/{$file}");
+            if ( ! \File::exists($targetFile)){
+                \File::copy(__DIR__ . "/../../../../database/migrations/{$file}", $targetFile);
+            }
+            $command->call('migrate');
+        }
+    }
+}
